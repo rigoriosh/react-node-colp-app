8af4649c14234334142860dcc5a88bd131d6a572
rigoriosh <rigoriosh@gmail.com>
2021-04-01 15:02:45 -0500

    fixes

--- cliente/src/hooks/UseAxios.js
+++ cliente/src/hooks/UseAxios.js
@@ -5,8 +5,8 @@ export const UseAxios = () => {
     const [respAxios, setAxios] = useState();
 
     const doRequeste = async(path, params) => {
-        //const url = `${process.env.REACT_APP_API_URL}${path}`;
-        const url = `http://localhost:8888${path}`;       
+        const url = `${process.env.REACT_APP_API_URL}${path}`;
+        //const url = `http://localhost:8888${path}`;       
         const resp = await axios.get(url, { params });
         setAxios(resp.data)
     } 
